3a5f4da28dc93e12c928343790f5765f98e44dae
Randy Heit <rheit@zdoom.fake>
2010-08-29 05:04:38 +0000

    - Put CMakeLists.txt in the source zip, not the old project files.
    SVN r2633 (trunk)

--- zipsrc.bat
+++ zipsrc.bat
@@ -1,4 +1,4 @@
-zip -9 %1 COPYING Makefile *.cpp *.h *.c *.rc *.ds? *.sln *.vcproj unused/* zlib/*.c zlib/*.h
-kzip b%1 COPYING Makefile *.cpp *.h *.c *.rc *.ds? *.sln *.vcproj unused/* zlib/*.c zlib/*.h
+zip -9 %1 COPYING Makefile *.cpp *.h *.c *.rc CMakeLists.txt unused/* zlib/*.c zlib/*.h zlib/CMakeLists.txt 
+kzip b%1 COPYING Makefile *.cpp *.h *.c *.rc CMakeLists.txt unused/* zlib/*.c zlib/*.h zlib/CMakeLists.txt 
 zipmix %1 b%1
 del b%1
