bd06fbd5d7620283453eca5c4ed3bddd87a9ba40
Randy Heit <rheit@zdoom.fake>
2010-08-29 04:09:27 +0000

    - Only include strings.h in getopt.c if __APPLE__ is defined.
    SVN r2631 (trunk)

--- getopt.c
+++ getopt.c
@@ -43,7 +43,9 @@ USA.  */
 #endif
 
 #include <stdio.h>
+#ifdef __APPLE__
 #include <strings.h>
+#endif
 
 /* Comment out all this code if we are using the GNU C Library, and are not
    actually compiling the library itself.  This code is part of the GNU C
